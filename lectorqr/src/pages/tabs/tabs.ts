ef2126afccf475294abbb9b6b102a726e2249494
Your Name <you@example.com>
2018-10-02 20:39:45 -0500

    Solucion en bug de @components de renderizado de pagina

--- lectorqr/src/pages/tabs/tabs.ts
+++ lectorqr/src/pages/tabs/tabs.ts
@@ -9,19 +9,18 @@ import { HomePage, GuardadosPage } from "../index.paginas";
  * Ionic pages and navigation.
  */
 
-/*
-@IonicPage()
+
+//@IonicPage()
 @Component({
   selector: 'page-tabs',
   templateUrl: 'tabs.html',
 })
-*/
 export class TabsPage {
   tab1:any = HomePage;
   tab2:any = GuardadosPage;
 
   constructor() {
-    
+
   }
   /*constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
